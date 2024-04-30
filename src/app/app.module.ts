b1f1c7ce35d9dc6fa19e9976d78cbb7bed05b218
Nedim Porcic <Nedim.Porcic@bossinfo.ch>
2024-04-30 11:49:24 +0200

    added Caloriecalculator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CaloriecalculatorComponent } from './caloriecalculator/caloriecalculator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CaloriecalculatorComponent,
     NavbarComponent
   ],
   imports: [
