fd10cc3ddd7b89b80151d70de5345fcc5207af91
Nedim Porcic <Nedim.Porcic@bossinfo.ch>
2024-05-12 21:38:20 +0200

    added Unittests and Logic for Caloriecalculator

--- src/app/caloriecalculator/caloriecalculator.component.ts
+++ src/app/caloriecalculator/caloriecalculator.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-caloriecalculator',
   templateUrl: './caloriecalculator.component.html',
-  styleUrl: './caloriecalculator.component.scss'
+  styleUrls: ['./caloriecalculator.component.scss']
 })
 export class CaloriecalculatorComponent {
+
+  calculateCalories(): void {
+    const form = document.getElementById('calorieForm') as HTMLFormElement;
+    const formData = {
+      age: parseInt((form.elements.namedItem('age') as HTMLInputElement).value),
+      gender: (form.elements.namedItem('Gender') as HTMLInputElement).value,
+      weight: parseInt((form.elements.namedItem('weight') as HTMLInputElement).value),
+      height: parseInt((form.elements.namedItem('height') as HTMLInputElement).value),
+      activity: parseFloat((form.elements.namedItem('activity') as HTMLSelectElement).value)
+    };
+
+    let bmr: number;
+    if (formData.gender === 'Male') {
+      bmr = 88.362 + (13.397 * formData.weight) + (4.799 * formData.height) - (5.677 * formData.age);
+    } else {
+      bmr = 447.593 + (9.247 * formData.weight) + (3.098 * formData.height) - (4.330 * formData.age);
+    }
+
+    const calories = Math.round(bmr * formData.activity);
+
+    const resultElement = document.getElementById('result') as HTMLElement;
+    resultElement.textContent = `Benötigte Kalorien pro Tag: ${calories}`;
+  }
 
 }
