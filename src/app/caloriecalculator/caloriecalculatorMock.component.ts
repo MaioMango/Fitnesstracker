94838e4ab6777c947beaa1ecf58b6b7752a094ab
Nedim Porcic <Nedim.Porcic@bossinfo.ch>
2024-05-14 11:36:17 +0200

    removed

--- src/app/caloriecalculator/caloriecalculatorMock.component.ts
+++ src/app/caloriecalculator/caloriecalculatorMock.component.ts
@@ -5,10 +5,8 @@ interface ICalorieCalculator {
     calculateCalories(age: number, gender: string, weight: number, height: number, activity: number): number;
   }
   
-  // Mock-Klasse 1
   export class MockCalorieCalculator1 implements ICalorieCalculator {
     calculateCalories(age: number, gender: string, weight: number, height: number, activity: number): number {
-      // Logik des ursprünglichen CalorieCalculator
       let bmr;
       if (gender === 'male') {
         bmr = 66.47 + (13.75 * weight) + (5.003 * height) - (6.755 * age);
