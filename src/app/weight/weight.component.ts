22dc8bf3514dcf437a3bd0adfae346a29deb79b8
Nedim Porcic <Nedim.Porcic@bossinfo.ch>
2024-05-21 11:36:10 +0200

    added weight to database

--- src/app/weight/weight.component.ts
+++ src/app/weight/weight.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-weight',
@@ -6,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrl: './weight.component.scss'
 })
 export class WeightComponent {
+  weight: number = 0;
+  weightForm: Date = new Date();
+  constructor(private formBuilder: FormBuilder, private http: HttpClient, private authService: AuthService) {}
 
+
+  save(): void {
+    const userid = this.authService.getIdFromToken();
+    const weight = this.weight;
+    const date = this.weightForm;
+
+    const weightData = { userid, weight, date };
+
+    this.http.post<any>('http://localhost:3000/weight', weightData).subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => {
+        console.error('Fehler beim Speichern des Gewicht:', error);
+      },
+    });
+  }
 }
+
+
